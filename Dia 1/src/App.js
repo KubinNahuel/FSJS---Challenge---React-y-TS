2ab43908f0c5685b72dbbf34b148b0140845bfdd
KubinNahuel <rn.kubin@hotmail.com>
2022-11-09 12:00:06 -0300

    Commit

---
+++ Dia 1/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Garage from './NuevosComponentes/primerComponente';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Garage/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
